aba6524db1e1ab6b227e25573961d4f60a61e25c
PE Lieb <lieb.pe@gmail.com>
2017-04-09 01:14:44 +0100

    [UPD] GET URL and logs

--- src/actions/trending-stars.js
+++ src/actions/trending-stars.js
@@ -12,8 +12,14 @@ export default async function trendingStars(res) {
   console.log(language)
   console.log('======================================')
   replies.push(formatter.formatMsg(res.reply()))
-  const response = await agent('GET', `GET https://api.github.com/search/repositories?q=language:${language}&sort=stars&order=desc`)
+  const response = await agent('GET', `https://api.github.com/search/repositories?q=language:${language}&sort=stars&order=desc`)
+  console.log('======================================')
+  console.log(response)
+  console.log('======================================')
   const repos = response.body.items.slice(0, 10)
+  console.log('======================================')
+  console.log(repos)
+  console.log('======================================')
   for (const rep in repos) {
     cardsReplies.push({
       name: rep.name,
